b248a2f6b59fb810bf1aa457ee1ecfdfed571e15
lolo23jhon <lolo23jhon@gmail.com>
2020-05-09 19:51:44 -0700

    Added class layout.

--- Entry.hpp
+++ Entry.hpp
@@ -16,13 +16,16 @@ using Episodes = std::vector<EpisodePtr>;
 enum class EntryType {
 	VIDEO,
 	MOVIE,
-	SEIRES_EPISODE
+	SERIES,
+	SERIES_EPISODE
 };
+
+
 
 class Entry {
 protected:
-	static int s_minRating;
-	static int s_maxRating;
+	static const int s_minRating;
+	static const int s_maxRating;
 
 	std::string m_name;
 	EntryType m_type;
@@ -30,22 +33,57 @@ protected:
 	Entry(const std::string& t_name, int t_rating);
 	
 
-	int getRating() const;
+	virtual void rate(int t_rating) const = 0;
+	virtual int getRating() const = 0;
+	virtual unsigned getLength() const = 0;
+
+
+public: 
 	const std::string& getName() const;
+	EntryType getType() const;
+};
+const int Entry::s_minRating{0};
+const int Entry::s_maxRating{5};
+const std::string& Entry::getName() const { return m_name; }
+EntryType Entry::getType() const { return m_type; }
+
+// Class for single elements such a move or video, in opposition to series which are collections
+class AtomicEntry : public Entry{
+protected:
+	int m_rating;
+	unsigned m_length;
+
+public:
+	int getRating() const;
+	unsigned getLength() const;
+};
+
+int AtomicEntry::getRating() const { return m_rating; }
+unsigned AtomicEntry::getLength() const { return m_length; }
+
+class CollectionEntry : public Entry {
 	
-	void rate(int t_rating);
+	int getRating() const = 0;
+	unsigned getLength() const = 0;
 };
 
-class Movie {
+
+
+
+
+class Movie :  public AtomicEntry {
+};
+
+class SeriesEpisode :  public AtomicEntry {
 
 };
 
-class SeriesEpisode {
+class Vide :  public AtomicEntry {
 
 };
 
-class Series {
-
+class Series : public CollectionEntry{
+	Episodes m_episodes;
 };
 
 #endif // !ENTRY_HPP
