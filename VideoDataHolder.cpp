2ec52f97bd684c41b469c3c674d75b6a39648ff6
lolo23jhon <lolo23jhon@gmail.com>
2020-05-10 18:17:42 -0700

    Added member functions to VideoDataHolder to add new movies and series.

--- VideoDataHolder.cpp
+++ VideoDataHolder.cpp
@@ -2,9 +2,17 @@
 
 VideoDataHolder& VideoDataHolder::addSeries(SeriesPtr t_series){
 	m_series.emplace_back(std::move(t_series));
-
+	std::vector<Episode*> episodes;
+	m_series.back()->getAllEpisodes(episodes);
+	for (auto ep : episodes) {
+		m_videos.emplace(ep->getId(), ep);
+	}
+	return *this;
 }
 
 VideoDataHolder& VideoDataHolder::addMovie(MoviePtr t_movie){
 	m_movies.emplace_back(std::move(t_movie));
+	auto mov{ m_movies.back().get() };
+	m_videos.emplace(mov->getId(), mov);
+	return *this;
 }
